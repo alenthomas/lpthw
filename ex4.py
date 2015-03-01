520e3983d809ab1a9bf8b7f982afc1de17bfe028
alenthomas <alenthomas9@gmail.com>
2015-03-01 15:36:28 +0000

    Add comments in ex4.py
    added comments for each of the variable assignments

--- ex4.py
+++ ex4.py
@@ -1,10 +1,18 @@
+#added variable car with value 100
 cars = 100
+#added variable space_in_a_car with value 4 in floating point
 space_in_a_car = 4.0
+#added variable drivers with value 30
 drivers = 30
+#added variable passengers with value 90
 passengers = 90
+#added varialbe cars_not_driven with value 100 - 30
 cars_not_driven = cars - drivers
+# assigned variable cas_driven with value drivers
 cars_driven = drivers
+# assigned variable carpool_capacity with value 30*4
 carpool_capacity = cars_driven * space_in_a_car
+# added variable average_passengers_per_car with value 90/30
 average_passengers_per_car = passengers / cars_driven
 
 
