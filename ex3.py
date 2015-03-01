3a8f950eb75a2cd47cf1a82ea6f1eb196d5fbf6c
alenthomas <alenthomas9@gmail.com>
2015-03-01 14:27:36 +0000

    Initial commit 3 exercise
    This is the initial commit.
    Adding all three ex{1..3}.py files together to the git repo

---
+++ ex3.py
@@ -0,0 +1,44 @@
+# This will print the line
+print "I will now count my chickens:"
+# This will first divide 30/6 and add the quotient with 25
+print "Hens", 25.0 + 30.0 / 6.0
+#This will multiply the remainder of 3/4 with 25 and subtract the product from 100
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0
+
+# This will print the line 
+print "Now I will count the eggs:"
+
+# This will evaluate (3+2+1-5)+(4%2)-(1/4)+6
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+# Print line
+print "Is it true that 3 + 2 < 5 - 7?"
+
+#evaluates the statement with true or false
+print 3 + 2 < 5 - 7
+
+# prints the line and then the arithmetic total
+print "What is 3 + 2?", 3.0 + 2.0
+#prints the line and then the difference
+print "What is 5 - 7?", 5.0 - 7.0
+
+#print line reference to earlier line
+print "Oh, that's why it's False."
+
+# print line
+print "How about some more."
+
+# prints line and then the evaluate statement as true
+print "Is it greater?", 5 > -2
+# prints line and then the evaluate statement as true
+print "Is it greater or equal?", 5 >= -2
+## prints line and then the evaluate statement as false
+print "Is it less or equal?", 5 <= -2
+
+print 7/4
+
+print 7.0/4.0
+
+print 4.0%2.0
+
+print 1.0/4.0
