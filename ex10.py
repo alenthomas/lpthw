9c7a81f05d0fcfe68529221bd8f241a569f760a2
alenthomas <alenthomas9@gmail.com>
2015-03-02 11:40:25 +0530

    Add comments strings and escape chracters
    Added Comments and other escape characters
    also appended a simple while and for loop to simulate a loading symbol

--- ex10.py
+++ ex10.py
@@ -1,15 +1,22 @@
+# Declared 3 strings
 tabby_cat = "\tI'm tabbed in."
 persian_cat = "I'm split\non a line."
 backslash_cat = "I'm \\ a \\ cat."
 
+#fourth string is declared using """
 fat_cat = """
 I'll do a list:
 \t* Cat food
 \t* Fishies
 \t* Catnip\n\t* Grass
 """
-
+#print the four strings
 print tabby_cat
 print persian_cat
 print backslash_cat
 print fat_cat
+
+#  a simple while loop in to animate/simulate a loading symbol
+while True:
+    for i in ["/", "-", "|", "\\","|"]:
+        print "%s\r" %i,
