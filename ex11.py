4419e8eebe190da6db9f225957a19a88304a95ed
alenthomas <alenthomas9@gmail.com>
2015-03-02 23:07:46 +0530

    Add new form raw_input()
    Added new raw_input() to ex11.py and print its values

--- ex11.py
+++ ex11.py
@@ -7,3 +7,10 @@ weight = raw_input()
 
 print "So, you're %r old, %r tall and %r heavy." % ( 
     age, height, weight)
+print "enter numeric for 'two':",
+two = raw_input()
+
+print "enter numeric for 'three':",
+three = raw_input()
+
+print "So two is %r and three is %r" %( two, three)
