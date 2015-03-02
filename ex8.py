93f17879f5c4abe8fc52136f017cfec9e7e433d6
alenthomas <alenthomas9@gmail.com>
2015-03-02 10:51:55 +0530

    Add comments to explain each %r

--- ex8.py
+++ ex8.py
@@ -1,9 +1,15 @@
+# declaring variable with a string of %r
 formatter = "%r %r %r %r"
 
+# prints formatter with output 1 2 3 4
 print formatter % (1, 2, 3, 4)
+# prints one two three four
 print formatter % ("one", "two", "three", "four")
+# prints true false false and true
 print formatter % (True, False, False, True)
+#prints %r %r %r %r four times
 print formatter % (formatter, formatter, formatter, formatter)
+# prints four strings
 print formatter % (
     "I had this thing.",
     "That you could type up right.",
