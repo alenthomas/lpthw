ef44c4f552ab721824f6f7ad16f66aa0a26e3a39
alenthomas <alenthomas9@gmail.com>
2015-03-03 12:37:41 +0530

    raw_input() with argv
    Implemented taking more input from the user using a combination of both
    raw_input() and argv

--- ex13.py
+++ ex13.py
@@ -2,8 +2,14 @@ from sys import argv
 
 script, first, second, three, four  = argv
 
-print "The script is called:", script
-print "Your first variable is :", first
-print "Your second variable is:", second
-print "Your third variable is:", three
-print "Your fourth variable is:", four
+#print "The script is called:", script
+print "Your first candidate is :", first
+#print "Your second variable is:", second
+#print "Your third variable is:", three
+#print "Your fourth variable is:", four
+
+print "\nEnter details of %r age, height" %(first),
+age = int(raw_input())
+height = int(raw_input())
+
+print "\n So candidate  %r with age %s and height %s" %( first, age, height)
