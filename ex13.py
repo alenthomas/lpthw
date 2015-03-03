a2521922da3e0d2ec0ab0bf6d60fe27e782af407
alenthomas <alenthomas9@gmail.com>
2015-03-03 12:19:11 +0530

    Add more argv variables
    Added more argv variables

--- ex13.py
+++ ex13.py
@@ -1,8 +1,9 @@
 from sys import argv
 
-script, first, second,  = argv
+script, first, second, three, four  = argv
 
 print "The script is called:", script
 print "Your first variable is :", first
 print "Your second variable is:", second
-#print "Your third variable is:", third
+print "Your third variable is:", three
+print "Your fourth variable is:", four
