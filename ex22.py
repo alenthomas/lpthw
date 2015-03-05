0492d258a355f7aecc1292bb9e48ed1f715314a0
alenthomas <alenthomas9@gmail.com>
2015-03-05 13:35:07 +0530

    Review Exercises till now

---
+++ ex22.py
@@ -0,0 +1,20 @@
+# print
+# comment
+# >= <= evaluates to true or false
+# %s, %r, (' ') (" ") ( """ """) value is given by %( values,set)
+# print + to concatenate ; * 'x'  to repeat the patter 'x' times
+# print and comma ',' after removes newline from print
+# raw_input() to get values ; can specify string inside ()
+# sys import argv 
+# to provide arguments to python script syntax var1, var2 = argv
+# open() open file for file operations
+# read() reads data from file obj
+# open( filename, 'mode') w for write mode ; default is r
+# truncate() just empties a file
+# write() writes to file
+# close() closes file
+# exists returns true is file already exists else returns false
+# def fun_name(): is the syntax to define a funcitons arguments inside ()
+# seek() seeks to the current file byte
+# return val;  returns the val to the control flow in def fun():
+#
