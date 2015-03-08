f2f993cb2849ff0f106ce5a780a7f814c964246e
alenthomas <alenthomas9@gmail.com>
2015-03-08 19:13:17 +0530

    List indexes
    Printing out list indexes

---
+++ ex34.py
@@ -0,0 +1,14 @@
+# list trials
+
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print "The list of animals is ", animals
+
+print "The animal at 1 : " ,animals[1]
+print "The animal at third : " ,animals[2]
+print "The animal at first : " ,animals[0]
+print "The animal at 3 : " ,animals[3]
+print "The animal at fifth : " ,animals[4]
+print "The animal at 2 : " ,animals[2]
+print "The animal at sixth : " ,animals[5]
+print "The animal at 4 : " ,animals[4]
