c1061c4dcdeb603a55437040bf3cd0c7708499d9
alenthomas <alenthomas9@gmail.com>
2015-03-08 18:52:09 +0530

    Add limit and increment value from user
    Modified the loop with a function for while-loop which takes the limit and
    increment values from user.

--- ex33.py
+++ ex33.py
@@ -1,15 +1,18 @@
-i = 0
 numbers = []
+limit = int (raw_input("Enter limit: "))
+inc = int ( raw_input("Enter incremet value: "))
+def whil(var, up):
+    i = 0
+    while i < var:
+        print "At the top i is %d" % i
+        numbers.append(i)
 
-while i < 6:
-    print "At the top i is %d" % i
-    numbers.append(i)
-
-    i = i + 1
-    print "Numbers now: ", numbers
-    print "At the bottom i is %d" % i
+        i = i + up
+        print "Numbers now: ", numbers
+        print "At the bottom i is %d" % i
 
 
+whil(limit, inc)
 print "The numbers: "
 
 for num in numbers:
